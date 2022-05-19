9855e0e1bfdc8920b9272f394b1f97e26fcd156f
Sehan Kim <sehandev@gmail.com>
2022-05-19 15:56:57 +0900

    FEAT: wifi list

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,40 +2,34 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
-const Hello = () => {
+type WifiArray = {
+  ssid: string;
+  password: string;
+};
+
+const Main = () => {
+  const wifiArray: WifiArray[] = [
+    { ssid: 'wifi1', password: 'abcd' },
+    { ssid: 'wifi2', password: 'abcd' },
+    { ssid: 'wifi3', password: 'abcd' },
+  ];
+
   return (
-    <div>
-      <div className="Hello">
+    <>
+      <div>
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
+      <h1 id="title">Wifi List</h1>
+      <ul>
+        {wifiArray.map((wifiInfo) => (
+          <li className="wifi-list">
+            <span>{wifiInfo.ssid}</span>
+            <span> - </span>
+            <span>{wifiInfo.password}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
@@ -43,7 +37,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
